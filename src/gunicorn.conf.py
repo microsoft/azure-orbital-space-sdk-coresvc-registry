8f51724cc5ed6dc37667413fbf424ee9be1ca2b8
Ryan Campbell <bigtallcampbell@gmail.com>
2024-07-23 00:41:17 +0000

    removing bind from config

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -8,5 +8,3 @@ worker_class = "gevent"
 # SSL Certs
 keyfile = "/certs/registry.spacefx.local.key"  # Path to your private key file
 certfile = "/certs/registry.spacefx.local.crt"  # Path to your certificate file
-
-bind = "0.0.0.0:8080"
